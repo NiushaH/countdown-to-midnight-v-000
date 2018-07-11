ced4b2e0c6bcb6758d8acb4563832c959622a25e
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-11 16:49:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
   end 
   puts "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end 
+  puts "HAPPY NEW YEAR!"
+end
